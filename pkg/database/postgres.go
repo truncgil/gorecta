ee3d4a9e3b01565e5ff5eeac69c23b731951ac6c
Ümit Tunç <umitreva@gmail.com>
2025-02-10 12:23:01 +0300

    Add PostgreSQL database connection utility functions

---
+++ pkg/database/postgres.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+// InitDB initializes the database connection
+func InitDB() (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSL_MODE"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %v", err)
+	}
+
+	log.Println("Successfully connected to database")
+	DB = db
+	return db, nil
+}
+
+// GetDB returns the database instance
+func GetDB() *gorm.DB {
+	return DB
+} 
